a5b41c06dd213a88047f7ed5194d0b8459466322
Rahul Soni <i.rahulsoni@outlook.com>
2022-04-24 16:28:08 -0400

    new recursion code

---
+++ recursion.py
@@ -0,0 +1,16 @@
+#Function calling itself for n number of times
+import sysc
+sys.setrecursionlimit(2000)
+
+i = 0
+def greet():
+    global  i
+    print("Hello ", i)
+    i +=1
+    greet()
+
+greet()
+
+
+print(sys.getrecursionlimit())
+
