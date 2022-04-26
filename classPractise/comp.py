1c78bb3d3b1615332f77a3920c7f8eb5b75e0e1b
Rahul Soni <i.rahulsoni@outlook.com>
2022-04-26 16:24:10 -0400

    Basic class practise with explanation of __init__ method

--- classPractise/comp.py
+++ classPractise/comp.py
@@ -1,15 +1,27 @@
-#User defined class
-class computer:
-    def config(self):
-        print("i5","16GB ", "1TB")
+# User defined class
+class Computer:
 
-comp = computer()
+    # Special method using __init__  to initiate variable, like constructor in other languages, It's called automatically
+    # For every object it is called at least once, so if we have 3 objects it will work 3 times automatically
+    # When __init__ is called we pass at least one value with it that's name of the object  which get assigned to the self
+    def __init__(self, cpu, ram):
+        self.cpu = cpu
+        self.ram = ram
+
+    # Instead of using the object name we just use self everytime we need to use the object which has been called
+    def config(self):
+        print(self.cpu, self.ram)
+
+
+# so when we call the method within the class we can pass the value of the class like follows:
+comp = Computer("i5", 16)
+comp2 = Computer('i7', 36)
 print(type(comp))
 
-#always mention the object name we want the function to perform on
-computer.config(comp)
-#It works the same way bcs config will take comp as argument
-comp.config()
+# always mention the object name we want the function to perform on
+# Computer.config(comp)
 
-a = 5
-print(a.bit_length())
+# When the class is called the name of object has been assigned to self argument
+# It works the same way bcs config will take comp as argument, more preferred way calling method from object
+comp.config()
+comp2.config()
