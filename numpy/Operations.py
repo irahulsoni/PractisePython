55977da47e808953114c9b650e06f1535be069cc
Rahul Soni <i.rahulsoni@outlook.com>
2022-06-01 13:25:37 -0400

    Bubble sort and other python data structures

---
+++ numpy/Operations.py
@@ -0,0 +1,22 @@
+# #Array with array
+# array with Scalars
+# Universal array functions
+
+import numpy as np
+
+arr = np.arange(0, 11)
+
+# Array on array operations:
+#adding 2 arrays, index by index
+arr1 = arr + arr
+
+#subrtracting 2 arrays
+arr1 = arr - arr
+
+arr1 = arr * arr
+arr1 = arr * 100
+
+arr1 = arr - 2
+
+
+
