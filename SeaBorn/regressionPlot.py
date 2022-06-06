692b43e7e9024b5f6b6e112e0ca035ab1ec159e3
Rahul Soni <i.rahulsoni@outlook.com>
2022-06-06 15:51:19 -0400

    Additional knowledge

--- SeaBorn/regressionPlot.py
+++ SeaBorn/regressionPlot.py
@@ -6,3 +6,8 @@ tips.head()
 # to have 2 seperate collumns for the fdata use col instead of hue, aspect and size to change the size of the table
 sns.lmplot(x='total_bill', y='tip', data= tips, col= 'sex',row='time', marker =['o','v'],scatter_kws ={'s': 100}, aspect= 0.6, size = 8)
 
+# style and color:
+#style: white give withe background, whitegrid, ticks
+sns.set_style('white')
+# remove the axes lines
+sns.despine(left=True)
