3d4d66a505e85f0bd3f8806d1fd3f175052bca48
Rahul Soni <i.rahulsoni@outlook.com>
2022-06-06 15:46:54 -0400

    Additional knowledge

---
+++ SeaBorn/regressionPlot.py
@@ -0,0 +1,8 @@
+import seaborn as sns
+
+tips = sns.load_dataset('tips')
+tips.head()
+# scattwe_kws is used to change the featurs of marhers, s is for size
+# to have 2 seperate collumns for the fdata use col instead of hue, aspect and size to change the size of the table
+sns.lmplot(x='total_bill', y='tip', data= tips, col= 'sex',row='time', marker =['o','v'],scatter_kws ={'s': 100}, aspect= 0.6, size = 8)
+
